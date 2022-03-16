e06d3bb67eb434fcbfbd80e087688c7e8b6e1405
Lohithry <92856243+Lohithry@users.noreply.github.com>
2022-03-16 12:01:32 -0400

    Add files via upload

---
+++ webanimation/index.js
@@ -0,0 +1,7 @@
+// Change the text between the quotation marks and run your code to see the name of your favorite language animated!
+drawText('Lohith Reddy');
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
+bounceBubbles();
+
